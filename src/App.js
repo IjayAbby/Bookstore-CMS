c6d3d82fa7a9a885bbfbc99de5ab716e49cd5c2b
IjayAbby <inyangasi4@gmail.com>
2021-09-04 02:45:31 +0300

    Switch Case

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/general/Navbar';
+import Categories from './components/categories/Categories';
+import MainBooks from './components/books/MainBooks';
 
-function App() {
-  return (
-    <div>
-     <h1 className="text-red-500">Hello there</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <MainBooks />
+      </Route>
+      <Route path="/category">
+        <Categories />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
