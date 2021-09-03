e0723a2909fe689b689e00330f4597edc15ec1f8
IjayAbby <inyangasi4@gmail.com>
2021-09-03 23:53:23 +0300

    Update component folder

---
+++ src/components/books/options.js
@@ -0,0 +1,69 @@
+const options = [
+    {
+      text: 'Science & Technology',
+      value: 'technology',
+      id: '1',
+    },
+    {
+      text: 'Historical',
+      value: 'history',
+      id: '2',
+    },
+    {
+      text: 'Tradition & Culture',
+      value: 'culture',
+      id: '3',
+    },
+    {
+      text: 'Romance',
+      value: 'romance',
+      id: '4',
+    },
+    {
+      text: 'Mythical',
+      value: 'mythical',
+      id: '5',
+    },
+    {
+      text: 'Fantacy',
+      value: 'fantacy',
+      id: '6',
+    },
+    {
+      text: 'Horror',
+      value: 'horror',
+      id: '7',
+    },
+    {
+      text: 'Documentary',
+      value: 'documentary',
+      id: '8',
+    },
+    {
+      text: 'Art & Craft',
+      value: 'art & craft',
+      id: '9',
+    },
+    {
+      text: 'Mental health',
+      value: 'mental health',
+      id: '10',
+    },
+    {
+      text: 'Motivational',
+      value: 'motivational',
+      id: '11',
+    },
+    {
+      text: 'Health',
+      value: 'health',
+      id: '12',
+    },
+    {
+      text: 'Guide/How-to',
+      value: 'guide',
+      id: '13',
+    },
+  ];
+  
+  export default options;
---
+++ src/components/categories/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const categories = () => {
+    return (
+        <div className="px-8 sm:px-16 lg:px-24 xl:px-28 bg-backg py-9">
+            <div className="container mx-auto">
+            Under construction
+            </div>
+      </div>
+    )
+}
+
+export default categories;
